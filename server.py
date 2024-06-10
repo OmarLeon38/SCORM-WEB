694b0449e1a0396b56e338dae0669f991cbda73f
Omar León <jleonp6@upao.edu.pe>
2024-06-10 17:07:13 -0500

    UPDATE HOST IP

--- server.py
+++ server.py
@@ -117,4 +117,4 @@ def confirmar():
         return jsonify({'status': 'Error', 'message': 'Contenido o selección no encontrados en la sesión'}), 400
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
